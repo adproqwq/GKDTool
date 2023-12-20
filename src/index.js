8760ab2d930b4c581745333866c4eabf33187174
adproqwq <2757917031@qq.com>
2023-12-21 01:26:14 +0800

    引入JSON5 API

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
-/*function getInput(){
-    var appName = document.getElementById('name').value;
-};
-*/
+const JSON5 = require('json5');
+
 function changeSwitch(index,job){
     var location = index.split('.');
     var i = location[0], j = location[1];
-    script = JSON.parse(script);
+    script = JSON5.parse(script);
     if(job == 'on'){
         if(script[i].groups[j].hasOwnProperty('enable') == true) delete script[i].groups[j].enable;
         else alert('该规则已经打开了');
@@ -24,7 +22,8 @@ function output(){
 
 var script;
 axios.get('../gkd.json5').then(function(data){
-    script = data.data.apps;
+    data = JSON5.parse(data.data);
+    script = data.apps;
     var initTable = `
     <table border="1">
         <thead>
@@ -38,16 +37,16 @@ axios.get('../gkd.json5').then(function(data){
         </thead>
         <tbody></tbody>
     </table>`;
-    document.getElementById('subVer').innerHTML = '<span>订阅版本：' + data.data.version + '</span>';
+    document.getElementById('subVer').innerHTML = '<span>订阅版本：' + data.version + '</span>';
     document.getElementById('appList').innerHTML = initTable;
     var eachAppRules = '';
-    for(var i in data.data.apps){
+    for(var i in data.apps){
         var packageName, appName, ruleName, desc;
-        packageName = data.data.apps[i].id;
-        appName = data.data.apps[i].name;
-        for(var j in data.data.apps[i].groups){
-            ruleName = data.data.apps[i].groups[j].name;
-            if(data.data.apps[i].groups[j].hasOwnProperty('desc') == true) desc = data.data.apps[i].groups[j].desc;
+        packageName = data.apps[i].id;
+        appName = data.apps[i].name;
+        for(var j in data.apps[i].groups){
+            ruleName = data.apps[i].groups[j].name;
+            if(data.apps[i].groups[j].hasOwnProperty('desc') == true) desc = data.apps[i].groups[j].desc;
             else desc = '该规则暂无描述';
             eachAppRules += `
             <tr>
