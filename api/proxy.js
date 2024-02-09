7da1835c3893f9eb6ba109de3938a5a4f09092bb
Adpro <101449500+adproqwq@users.noreply.github.com>
2024-02-10 01:19:04 +0800

    fix: try to fix 403

--- api/proxy.js
+++ api/proxy.js
@@ -13,7 +13,7 @@ module.exports = (req, res) => {
             '^/adpro/([^/]*)/': '/'
         },
         onProxyReq: (proxyReq, req, res) => {
-            proxyReq.setHeader('Cookie', 'cf_clearance=3NgsD2I_jm6qYmB1CDDop63txvvRERy9ah1C.JBAv7I-1706882981-1-AV40WoWYPPfZtp1V/NTOUiFCGg2NftndJY2RdArVKzQ+5wuJ9Oj3qjSrclxNbrapoVCnkQ3Kz2jEm7gHp8iY9VY=');
+            proxyReq.setHeader('cookie', 'cf_clearance=3NgsD2I_jm6qYmB1CDDop63txvvRERy9ah1C.JBAv7I-1706882981-1-AV40WoWYPPfZtp1V/NTOUiFCGg2NftndJY2RdArVKzQ+5wuJ9Oj3qjSrclxNbrapoVCnkQ3Kz2jEm7gHp8iY9VY=');
         }
     })(req, res)
 }
