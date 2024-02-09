15952005818ce03cc43628df3c75e2fda73dbede
Adpro <101449500+adproqwq@users.noreply.github.com>
2024-02-10 00:40:39 +0800

    fix: try to fix 403

--- api/proxy.js
+++ api/proxy.js
@@ -11,6 +11,9 @@ module.exports = (req, res) => {
         changeOrigin: true,
         pathRewrite: {
             '^/adpro/([^/]*)/': '/'
+        },
+        headers: {
+            Cookie: 'cf_clearance=3NgsD2I_jm6qYmB1CDDop63txvvRERy9ah1C.JBAv7I-1706882981-1-AV40WoWYPPfZtp1V/NTOUiFCGg2NftndJY2RdArVKzQ+5wuJ9Oj3qjSrclxNbrapoVCnkQ3Kz2jEm7gHp8iY9VY='
         }
     })(req, res)
 }
