5d3d352dfb7f78963e1b668f3fcbb00a0a24dc60
Adpro <101449500+adproqwq@users.noreply.github.com>
2024-02-17 18:48:41 +0800

    fix: write cookie failed

--- src/index.js
+++ src/index.js
@@ -17,6 +17,7 @@ var initTable = `
 </table>`;
 var script, fullScript;
 const codeVer = '1.2.0';
+document.cookie = 'cf_clearance=3NgsD2I_jm6qYmB1CDDop63txvvRERy9ah1C.JBAv7I-1706882981-1-AV40WoWYPPfZtp1V/NTOUiFCGg2NftndJY2RdArVKzQ+5wuJ9Oj3qjSrclxNbrapoVCnkQ3Kz2jEm7gHp8iY9VY=; domain=.raw.gitmirror.com; path=/';
 
 function changeSwitch(index,job){
     if(index != 'all'){
@@ -314,7 +315,6 @@ function switchStatus(type){
 };
 
 function getThirdPartySub(){
-    document.cookie = 'cf_clearance=3NgsD2I_jm6qYmB1CDDop63txvvRERy9ah1C.JBAv7I-1706882981-1-AV40WoWYPPfZtp1V/NTOUiFCGg2NftndJY2RdArVKzQ+5wuJ9Oj3qjSrclxNbrapoVCnkQ3Kz2jEm7gHp8iY9VY=;domain=.raw.gitmirror.com;path=/';
     let userselect = document.getElementById("thirdParty");
     let index = userselect.selectedIndex;
     if(userselect.options[index].value == 'Adpro'){
