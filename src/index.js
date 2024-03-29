dea0f84ff3ef45d9f3f52d62d8ba5aeef1c0a555
Adpro <101449500+adproqwq@users.noreply.github.com>
2024-03-29 23:59:18 +0800

    feat: getThirdPartySub use jQuery

--- src/index.js
+++ src/index.js
@@ -297,28 +297,28 @@ function getThirdPartySub() {
   let userselect = document.getElementById("thirdParty");
   let index = userselect.selectedIndex;
   if (userselect.options[index].value == 'Adpro') {
-    axios.get('/adpro/cdn/gh/Adpro-Team/GKD_subscription@main/dist/Adpro_gkd.json5').then((data) => {
+    $.get('https://raw.gitmirror.com/Adpro-Team/GKD_subscription/main/dist/Adpro_gkd.json5', (data) => {
       data = JSON5.parse(data.data);
       writeTable(data);
       alert('导入成功！');
     });
   }
   else if (userselect.options[index].value == 'AIsouler') {
-    axios.get('/adpro/cdn/gh/AIsouler/subscription@main/dist/AIsouler_gkd.json5').then((data) => {
+    $.get('https://raw.gitmirror.com/AIsouler/GKD_subscription/main/dist/AIsouler_gkd.json5', (data) => {
       data = JSON5.parse(data.data);
       writeTable(data);
       alert('导入成功！');
     });
   }
   else if (userselect.options[index].value == 'aoguai') {
-    axios.get('/adpro/cdn/gh/aoguai/subscription@custom/dist/aoguai_gkd.json5').then((data) => {
+    $.get('https://raw.gitmirror.com/aoguai/subscription/custom/dist/aoguai_gkd.json5', (data) => {
       data = JSON5.parse(data.data);
       writeTable(data);
       alert('导入成功！');
     });
   }
   else if (userselect.options[index].value == 'ganlinte') {
-    axios.get('/adpro/cdn/gh/ganlinte/GKD-subscription@main/dist/ganlin_gkd.json5').then((data) => {
+    $.get('https://raw.gitmirror.com/ganlinte/GKD-subscription/main/dist/ganlin_gkd.json5', (data) => {
       data = JSON5.parse(data.data);
       writeTable(data);
       alert('导入成功！');
